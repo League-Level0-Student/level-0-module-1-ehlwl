7bd05806f0d81fd0dcbccedea7f8738f41f53fa4
ehlwl <qkd3821@gmail.com>
2020-03-21 15:53:43 -0700

    Create badgersong.java

---
+++ src/_12_badgers/badgersong.java
@@ -0,0 +1,22 @@
+package _12_badgers;
+
+public class badgersong {
+	public static void main(String[] args) {
+		for (int l=0;l<3;l++) {
+		for (int i=0; i < 5; i++) {
+			System.out.print("badger"+" ");
+		}
+		System.out.println();
+		for (int d=0; d <2; d++) {
+			for (int o = 0; o <3; o++) {
+				System.out.print("mushroom"+" ");
+			}
+		 System.out.println();
+		}
+		for (int j=0;j<7;j++) {
+			System.out.print("snake"+" ");
+		}
+		}
+	}
+
+}
